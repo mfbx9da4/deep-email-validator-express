b8cd79d0da9354e2a01bc8e5fc484f8f749724a2
David Adler <dalberto.adler@gmail.com>
2020-03-09 18:37:53 +0000

    added tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import validate from 'deep-email-validator'
 import http from 'http'
-import express from 'express'
+import express, { Response } from 'express'
 import path from 'path'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
@@ -16,14 +16,21 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.post('/email/validate', async (req, res) => {
-  const { email } = req.body
-  console.log('req.body', req.body)
+const validateEndpoint = async (options: any, res: Response<unknown>) => {
+  const { email } = options
   if (!email)
     return res.status(400).send({ error: { name: 'Email not provided' } })
   const result = await validate(email)
   console.log('result', result)
   res.send(result)
+}
+
+app.post('/email/validate', async (req, res) => {
+  return validateEndpoint(req.body, res)
+})
+
+app.get('/email/validate', async (req, res) => {
+  return validateEndpoint(req.query, res)
 })
 
 if (app.get('env') === 'development') {
@@ -34,3 +41,5 @@ const server = http.createServer(app)
 server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'))
 })
+
+export default server
